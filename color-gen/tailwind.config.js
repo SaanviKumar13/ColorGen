bc580fe978d41d1f54e50d79057b3b8f09dac537
SaanviKumar13 <saanviprashantkumar@gmail.com>
2023-07-29 11:18:12 +0530

    fix: changing from app/ to pages/

--- color-gen/tailwind.config.js
+++ color-gen/tailwind.config.js
@@ -7,10 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily:{
-        'body':['Playfair Display', 'serif'],
-        'title':['Abril Fatface', 'cursive'],
-      }
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
   },
   plugins: [],
